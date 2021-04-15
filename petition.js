dae10213bd88a396ed686a1e05ae5682abe34891
Miguelhyto <miguelhito54@gmail.com>
2021-04-15 13:12:58 -0500

    font,form1,color user

--- petition.js
+++ petition.js
@@ -1,12 +1,27 @@
 const BASE_URL = 'http://test.movilbox.co:888/test_mbox/test.php?metodo='
   let xhr
+  let res = document.getElementById('res')
   
   if(window.XMLHttpRequest)xhr = new XMLHttpRequest()
   else xhr = new ActiveXObject("Microsoft.XMLHTTP")
   
   xhr.open('GET', BASE_URL + 'usuarios')
   xhr.addEventListener('load',(users) =>{
-    data=JSON.parse(users.target.response)
+  data=JSON.parse(users.target.response)
+    for(let i of data){
+      res.innerHTML +=`
+        <div class='user-container'>
+          ${i.nombre}
+          <br>
+          ${i.perfil}
+          <h4>dias planificados</h4>
+           ${i.dias_plani}
+          <h4>tiendas planificadas</h4>
+           ${i.tiendas_plani}
+        </div>
+
+      `
+    }
   })
   xhr.send()
   
---
+++ usercolor.js
@@ -0,0 +1,3 @@
+function getColor(){
+    let 
+}
