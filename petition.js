50840c1a9e83b383a298d3d79b9b7ec50858d48d
Miguelhyto <miguelhito54@gmail.com>
2021-04-14 22:03:14 -0500

    Ajax-Json
    peticiones ajax

---
+++ petition.js
@@ -0,0 +1,17 @@
+const button = document.getElementById('button')
+
+button.addEventListener("click", () => {
+  
+  const BASE_URL = 'http://test.movilbox.co:888/test_mbox/test.php?metodo='
+  let xhr
+  
+  if(window.XMLHttpRequest)xhr = new XMLHttpRequest()
+  else xhr = new ActiveXObject("Microsoft.XMLHTTP")
+  
+  xhr.open('GET', BASE_URL + 'usuarios')
+  xhr.addEventListener('load',(users) =>{
+    data=JSON.parse(users.target.response)
+  })
+  xhr.send()
+  
+})
