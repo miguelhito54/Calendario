8a76f7077c646db6467256de3cdc25915a1203d3
Miguelhyto <miguelhito54@gmail.com>
2021-04-16 20:33:24 -0500

    calendario 70% sin cuadriculas

--- calendar.js
+++ calendar.js
@@ -1,29 +1,91 @@
-let xhrp
+let currentDate = new Date();
+let currentDay = currentDate.getDate();
+let monthNumber = currentDate.getMonth();
+let currentYear = currentDate.getFullYear();
+
 let prevmonth = document.getElementById('prev');
+
+let monthNames=['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre','Octubre', 'Noviembre', 'Diciembre'];
 let month = document.getElementById('month');
 let year = document.getElementById('year')
+
 let nextmonth = document.getElementById('next');
 
+let dates = document.getElementById('number')
 
-  if(window.XMLHttpRequest)xhrp = new XMLHttpRequest()
-  else xhrp = new ActiveXObject("Microsoft.XMLHTTP")
-  xhrp.open('GET', BASE_URL + 'periodos')
-  xhrp.addEventListener('load',(periodo) =>{
-  date=JSON.parse(periodo.target.response)
-  //calendar
-for(let item of date){
-      month.textContent = item.mes;
-      year.textContent = item.anio;      
-}
-function writeMonth(){
-      for(let i = 1 ; i <31; i++){
-      date.innerHTML += 
-      `<div class=''>${i}</div>`
+month.textContent = monthNames[monthNumber];
+year.textContent = currentYear.toString();
+//flecha izquierda, derecha
+prevmonth.addEventListener('click', ()=>lastMonth());
+nextmonth.addEventListener('click', ()=>nextMonth());
+
+const writeMonth = (month) => {
+   for(let i = startDay(); i>0;i--){
+        dates.innerHTML += ` <div class="calendar__date calendar__item calendar__last-days">
+            ${getTotalDays(monthNumber-1)-(i-1)}
+        </div>`;
+    }
+     for(let i=1; i<=getTotalDays(month); i++){
+        if(i===currentDay) {
+            dates.innerHTML += ` <div>${i}</div>`;
+        }else{
+            dates.innerHTML += ` <div>${i}</div>`;
         }
-      }
-})
+    }
+}
 
-   xhrp.send()
+const getTotalDays = (month) => {
+    if(month === -1) month = 11;
+
+    if (month == 0 || month == 2 || month == 4 || month == 6 || month == 7 || month == 9 || month == 11) {
+        return  31;
+
+    } else if (month == 3 || month == 5 || month == 8 || month == 10) {
+        return 30;
+
+    } else {
+
+        return isLeap() ? 29:28;
+    }
+}
+
+const isLeap = () => {
+    return ((currentYear % 100 !==0) && (currentYear % 4 === 0) || (currentYear % 400 === 0));
+}
+
+function startDay(){
+    let start = new date(currentYear,monthNumber,1);
+    return (start.getDay())
+}
+function lastMonth(){
+    if(monthNumber !== 0){
+      monthNumber--;
+    }else{
+      monthNumber = 11;
+      currentYear--;
+    }
+      setNewDate();
+}
+function nextMonth(){
+  if(monthNumber !== 0){
+      monthNumber++;
+    }else{
+      monthNumber = 11;
+      currentYear++;
+    }
+      setNewDate();
+    
+}
+const setNewDate = () => {
+    currentDate.setFullYear(currentYear,monthNumber,currentDay);
+    month.textContent = monthNames[monthNumber];
+    year.textContent = currentYear.toString();
+    dates.textContent = '';
+    writeMonth(monthNumber);
+}
+
+
+
    
 
 
