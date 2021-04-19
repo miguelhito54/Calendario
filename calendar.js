76cb2fa9d553362d99647116d7b554f4ef775243
Miguelhyto <miguelhito54@gmail.com>
2021-04-19 16:21:13 -0500

    ñ

--- calendar.js
+++ calendar.js
@@ -1,26 +1,23 @@
+let monthNames = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre','Octubre', 'Noviembre', 'Diciembre'];
+let prevmonth = document.getElementById('prev');
+let month = document.getElementById('month');
+let year = document.getElementById('year')
+let nextmonth = document.getElementById('next');
+let dates = document.getElementById('number')
 let currentDate = new Date();
 let currentDay = currentDate.getDate();
 let monthNumber = currentDate.getMonth();
 let currentYear = currentDate.getFullYear();
 
-let prevmonth = document.getElementById('prev');
-
-let monthNames=['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre','Octubre', 'Noviembre', 'Diciembre'];
-let month = document.getElementById('month');
-let year = document.getElementById('year')
-
-let nextmonth = document.getElementById('next');
-
-let dates = document.getElementById('number')
-
-month.textContent = monthNames[monthNumber];
+month.textContent = monthNames [monthNumber];
 year.textContent = currentYear.toString();
-//flecha izquierda, derecha
+
 prevmonth.addEventListener('click', ()=>lastMonth());
 nextmonth.addEventListener('click', ()=>nextMonth());
 
 const writeMonth = (month) => {
-   for(let i = startDay(); i>0;i--){
+   
+    for(let i = startDay(); i>0;i--){
         dates.innerHTML += ` <div>
             ${getTotalDays(monthNumber-1)-(i-1)}
         </div>`;
@@ -31,6 +28,7 @@ const writeMonth = (month) => {
     }
 
 const getTotalDays = (month) => {
+    
     if(month === -1) month = 11;
 
     if (month == 0 || month == 2 || month == 4 || month == 6 || month == 7 || month == 9 || month == 11) {
@@ -46,14 +44,17 @@ const getTotalDays = (month) => {
 }
 
 const isLeap = () => {
+    
     return ((currentYear % 100 !==0) && (currentYear % 4 === 0) || (currentYear % 400 === 0));
 }
 
 function startDay(){
+    
     let start = new date(currentYear,monthNumber,1);
     return (start.getDay())
 }
 function lastMonth(){
+    
     if(monthNumber !== 0){
       monthNumber--;
     }else{
@@ -63,21 +64,24 @@ function lastMonth(){
       setNewDate();
 }
 function nextMonth(){
-  if(monthNumber !== 0){
+  
+    if(monthNumber !== 11){
       monthNumber++;
     }else{
-      monthNumber = 11;
+      monthNumber = 0;
       currentYear++;
     }
       setNewDate();
     
 }
 const setNewDate = () => {
+    
     currentDate.setFullYear(currentYear,monthNumber,currentDay);
     month.textContent = monthNames[monthNumber];
     year.textContent = currentYear.toString();
     dates.textContent = '';
     writeMonth(monthNumber);
+
 }
 
 
--- petition.js
+++ petition.js
@@ -1,8 +1,8 @@
 const BASE_URL = 'http://test.movilbox.co:888/test_mbox/test.php?metodo='
-  let xhr
-  let res = document.getElementById('res')
-  
-  if(window.XMLHttpRequest)xhr = new XMLHttpRequest()
+let res = document.getElementById('res')  
+let xhr
+ 
+if(window.XMLHttpRequest)xhr = new XMLHttpRequest()
   else xhr = new ActiveXObject("Microsoft.XMLHTTP")
   
   xhr.open('GET', BASE_URL + 'usuarios')
